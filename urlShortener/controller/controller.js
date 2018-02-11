4b94c19820aad5ec005a770e3f01a4309c097e8e
DanielVG <danielveragi@gmail.com>
2018-02-11 18:49:56 +0100

    started with routes

--- urlShortener/controller/controller.js
+++ urlShortener/controller/controller.js
@@ -5,5 +5,12 @@ let bodyParser = require("body-parser");
 let express = require("express");
 let router = express.Router();
 
+//router to make new shortened URl
+router.get('/new/:url(*)', (req, res, next) => {
+
+})
+
+//router to query existing URL
+router.get('/')
 
 module.exports = router;
