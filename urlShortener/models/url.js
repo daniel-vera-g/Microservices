3f8ddac6c623224a66a540b0e789cbe12a1273c7
dvg <danielveragi@gmail.com>
2018-02-14 09:16:34 +0100

    url model
    Signed-off-by: dvg <danielveragi@gmail.com>

--- urlShortener/models/url.js
+++ urlShortener/models/url.js
@@ -3,19 +3,18 @@
  * @create date 2018-02-14 09:13:36
  * @modify date 2018-02-14 09:13:36
  * @desc Scheema for MongoDB
-*/
+ */
 
 const mongoose = require("mongoose");
 
-
 //schema constructer
 let Schema = mongoose.Schema;
+
 //url shema
-let urlSchema = new Schema(
-  {
-    originalUrl: string,
-    shortUrl: string
-    }
-);
+let urlSchema = new Schema({
+  originalUrl: string,
+  shortUrl: string
+});
+
 //create model from Schema
-let links = mongoose.model("links", urlSchema);
+module.exports = mongoose.model("links", urlSchema);
