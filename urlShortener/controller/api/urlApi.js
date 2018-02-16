81c69dc48593a49fd4d531efd91a98252a14a208
dvg <danielveragi@gmail.com>
2018-02-16 14:37:26 +0100

    debug
    Signed-off-by: dvg <danielveragi@gmail.com>

--- urlShortener/controller/api/urlApi.js
+++ urlShortener/controller/api/urlApi.js
@@ -5,9 +5,9 @@
  * @desc API for the URL Shortener
  */
 
-const debug = require('debug')('DEBUG:urlAPI');
+const debug = require("debug")("DEBUG:urlAPI");
 let ex = module.exports;
-const links = require('../../models/url');
+const links = require("../../models/url");
 
 //Short Id to generate unique id
 const shortId = require("shortid");
@@ -19,18 +19,27 @@ shortId.characters(
 /**
  * Add short URL to the DB
  * @param  {String} longUrl original URl passed by the user
- */ 
-ex.addShortUrl = async (longUrl) => {
+ */
+
+ex.addShortUrl = async longUrl => {
   return new Promise((acc, rej) => {
+    debug("adding short url with the longURL %s", longUrl);
     //generate short url
     let id = shortId.generate();
-    let host = 'localhost:3000/';
+    let host = "localhost:3000/";
 
     //create & save new link instance
-    links.create({ _id: id, originalUrl: longUrl, shortUrl: host + id}, (err, _id) => {
-      if (err) rej(err);
-      acc(_id);
-    });
+    links.create(
+      { _id: id, originalUrl: longUrl, shortUrl: host + id },
+      (err, _id) => {
+        if (err) {
+          debug("ERROR creating new short URl in the DB with the longURL %S", longUrl);
+          rej(err);
+        }
+        debug("Short url with the id %s created", id);
+        acc(_id);
+      }
+    );
   });
 };
 
@@ -39,7 +48,7 @@ ex.addShortUrl = async (longUrl) => {
  * @param  {string} shortUrl short URl created with the long URL
  * @returns original Url
  */
-ex.getLongURl = async (shortUrl) => {
+ex.getLongURl = async shortUrl => {
   return new Promise((acc, rej) => {});
 };
 
@@ -48,7 +57,7 @@ ex.getLongURl = async (shortUrl) => {
  * @param  {int} UrlId Id of the Short URL in the DB
  * @returns the short url from the id
  */
-ex.getShortUrl = async (UrlId) => {
+ex.getShortUrl = async UrlId => {
   return new Promise((acc, rej) => {});
 };
 
@@ -57,7 +66,7 @@ ex.getShortUrl = async (UrlId) => {
  * @param  {String} url URL given by the User
  * @requires booljean to see if url is valid or not
  */
-ex.validateUrl = async (url) => {
+ex.validateUrl = async url => {
   return new Promise((acc, rej) => {});
 };
 
