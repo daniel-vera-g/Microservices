140f916ba66b17dc0627b2b7785caa8247ebcfbf
danielVg <danielveragi@gmail.com>
2018-07-18 16:19:59 +0200

    form errrors
    Signed-off-by: danielVg <danielveragi@gmail.com>

--- requestHeaderMicroservice/server.js
+++ requestHeaderMicroservice/server.js
@@ -6,39 +6,39 @@
 const http = require("http");
 
 // Express
-const express = require('express');
+const express = require("express");
 
 // Express middleqare
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const path = require("path");
 
 const router = express.Router();
 
-//make the routes
-let routes = require('./controller/controller');
+// make the routes
+const routes = require("./controller/controller");
 
-let app = express();
+const app = express();
 
 // BodyParser Middleware
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-//Body pareser middleware
+// Body pareser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//middleware for the routes
-app.use('/', routes);
-app.use('/:data', routes);
+// middleware for the routes
+app.use("/", routes);
+app.use("/:data", routes);
 
 // set the port
-app.set('port', (process.env.PORT || 3000));
+app.set("port", (process.env.PORT || 3000));
 
 // listen to port
-app.listen(app.get('port'),'127.0.0.1',  function(){
+app.listen(app.get("port"), "127.0.0.1", () => {
 	console.log('Server started at port' + app.get('port'));
 });
 
