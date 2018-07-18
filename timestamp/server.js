0ffd48e0e040057fd6b495b3881cbb5f54454130
danielVg <danielveragi@gmail.com>
2018-07-18 16:22:54 +0200

    removed unused dependencies
    Signed-off-by: danielVg <danielveragi@gmail.com>

--- timestamp/server.js
+++ timestamp/server.js
@@ -6,49 +6,49 @@
 const http = require("http");
 
 // Express
-const express = require('express');
+const express = require("express");
 
 // Express middleqare
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const exphbs = require('express-handlebars');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const exphbs = require("express-handlebars");
 const path = require("path");
 
 const router = express.Router();
 
-//make the routes
-let routes = require('./controller/controller');
+// make the routes
+const routes = require("./controller/controller");
 
-let app = express();
+const app = express();
 
 // BodyParser Middleware
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 
 // Insert static content here - before the dynamic one
 // Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // View Engine
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'layouts'}));
-app.set('view engine', 'handlebars');
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs({ defaultLayout: "layouts" }));
+app.set("view engine", "handlebars");
 
-//Body pareser middleware
+// Body pareser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//middleware for the routes
-app.use('/', routes);
+// middleware for the routes
+app.use("/", routes);
 
 // set the port
-app.set('port', (process.env.PORT || 3000));
+app.set("port", (process.env.PORT || 3000));
 
 // listen to port
-app.listen(app.get('port'), function(){
+app.listen(app.get("port"), () => {
 	console.log('Server started at port' + app.get('port'));
 });
 
