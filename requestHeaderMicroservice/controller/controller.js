24496c30af6dd095f2c305d8443cb3f61ec2ed25
danielVg <danielveragi@gmail.com>
2018-07-18 16:20:36 +0200

    form errors in the controller
    Signed-off-by: danielVg <danielveragi@gmail.com>

--- requestHeaderMicroservice/controller/controller.js
+++ requestHeaderMicroservice/controller/controller.js
@@ -1,40 +1,41 @@
-let bodyParser = require("body-parser");
-let express = require("express");
-let router = express.Router();
+const bodyParser = require("body-parser");
+const express = require("express");
 
-//home handler
+const router = express.Router();
+
+// home handler
 router.get("/", (req, res, next) => {
-  //create array with information
-  let information = {
-    ipAdress: null,
-    language: null,
-    operatingSystem: null
-  };
+	// create array with information
+	const information = {
+		ipAdress: null,
+		language: null,
+		operatingSystem: null,
+	};
 
-  let reqHeader = req.headers;
-  console.log(reqHeader);
+	const reqHeader = req.headers;
+	console.log(reqHeader);
 
-  //language
-  let reqLanguage = reqHeader["accept-language"].split(",")[0].trim();
-  console.log(reqLanguage);
-  information.language = reqLanguage;
+	// language
+	const reqLanguage = reqHeader["accept-language"].split(",")[0].trim();
+	console.log(reqLanguage);
+	information.language = reqLanguage;
 
-  //OS
-  let reqOs = reqHeader["user-agent"];
-  //get only thing in brackets and take the first parameter
-  reqOs = reqOs.split(/[\(\)]/)[1];
-  console.log(reqOs);
-  //trim to remove whitespaces
-  reqOs.trim();
-  console.log(reqOs);
-  information.operatingSystem = reqOs;
+	// OS
+	let reqOs = reqHeader["user-agent"];
+	// get only thing in brackets and take the first parameter
+	reqOs = reqOs.split(/[\(\)]/)[1];
+	console.log(reqOs);
+	// trim to remove whitespaces
+	reqOs.trim();
+	console.log(reqOs);
+	information.operatingSystem = reqOs;
 
-  //get ipAdress, language & operating system from request
-  information.ipAdress =
+	// get ipAdress, language & operating system from request
+	information.ipAdress =
     req.header("x-forwarded-for") || req.connection.remoteAddress;
 
-  //send information back to client
-  res.send(information);
+	// send information back to client
+	res.send(information);
 });
 
 module.exports = router;
