0133ceec0299f2b29ac0c7336756dd3b40461d59
danielVg <danielveragi@gmail.com>
2018-07-18 16:27:23 +0200

    refactoring
    Signed-off-by: danielVg <danielveragi@gmail.com>

--- urlShortener/controller/routes/routes.js
+++ urlShortener/controller/routes/routes.js
@@ -7,54 +7,55 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const debug = require('debug')('DEBUG:routes');
+const debug = require("debug")("DEBUG:routes");
 const urlAPI = require("../api/urlApi");
+
 const router = express.Router();
 
-//home route
+// home route
 router.get("/", (req, res, next) => {
-  res.render("index");
+	res.render("index");
 });
 
-//create and return short url from longUrl
+// create and return short url from longUrl
 router.get("/new/:url(*)", async (req, res, next) => {
-  let url = req.params.url;
-  debug(url);
-  
-  debug("The url given by the user is %s", url);
+	const { url } = req.params;
+	debug(url);
 
-  //check if URL is valid
-  debug("Checking if the url is in the right format");
-  let isValid = await urlAPI.validateUrl(url);
-  debug("The url format is: ", isValid);
+	debug("The url given by the user is %s", url);
 
-  if (isValid) {
-    //call the api to create short url
-    let shortUrl = await urlAPI.addShortUrl(url);
-    res.send('The short url is: ' + shortUrl);
-  } else {
-    res.send("Url not valid. Please try another time with a valid url");
-  }
+	// check if URL is valid
+	debug("Checking if the url is in the right format");
+	const isValid = await urlAPI.validateUrl(url);
+	debug("The url format is: ", isValid);
+
+	if (isValid) {
+		// call the api to create short url
+		const shortUrl = await urlAPI.addShortUrl(url);
+		res.send(`The short url is: ${  shortUrl}`);
+	} else {
+		res.send("Url not valid. Please try another time with a valid url");
+	}
 });
 
 // redirect user to original url given the short url
 router.get("/:shortened", async (req, res, next) => {
-    let shortLink = req.params.shortened;
-    debug('requesting for a short link %s', shortLink);
+	const shortLink = req.params.shortened;
+	debug("requesting for a short link %s", shortLink);
 
-    //check if url exists and get url from the database
-    debug('quering the original url for %s', shortLink);
-    let urlExistence = await urlAPI.getLongURl(shortLink);
-    debug('The original url is %s', urlExistence);
+	// check if url exists and get url from the database
+	debug("quering the original url for %s", shortLink);
+	const urlExistence = await urlAPI.getLongURl(shortLink);
+	debug("The original url is %s", urlExistence);
 
-    if(urlExistence == false) {
-        debug('The existence of the url is %s', urlExistence);
-        res.send('Short link does not exist. Please check the link or create one first');
-    }else{
-        res.redirect(urlExistence);
-    }
+	if (urlExistence == false) {
+		debug("The existence of the url is %s", urlExistence);
+		res.send("Short link does not exist. Please check the link or create one first");
+	} else {
+		res.redirect(urlExistence);
+	}
 
-  //redirect the user
+	// redirect the user
 });
 
 module.exports = router;
